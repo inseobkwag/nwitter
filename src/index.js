a97f267b6e5c68a75ccd51b9d1352d2202123af4
inseobkwag <kis970820@gmail.com>
2022-03-31 19:41:08 +0900

    2022-03-30

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+console.log(firebase)
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
